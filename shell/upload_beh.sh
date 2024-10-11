287f1b6d91d92515b61a58d01443246324b31272
Monica Thieu <mthieu@emory.edu>
2024-10-11 16:33:30 -0400

    update onedrive path

--- shell/upload_beh.sh
+++ shell/upload_beh.sh
@@ -6,6 +6,6 @@ read -p "Enter subj num(s) to heuristic with no leading 0s (but with spaces): " 
 for SUBJ_NUM in ${SUBJ_NUMS[@]}; do
     printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
 # can't scp into symlinked folders, must go to the original
-    scp ~/OneDrive\ -\ Emory\ University/PsychoPy/looming_nback_psy/data/sub-${SUBJ_NUM_4D}/*.csv ecco:/archival/projects/SPLaT/data/beh/sub-${SUBJ_NUM_4D}/raw/
-    scp ~/OneDrive\ -\ Emory\ University/PsychoPy/looming_stimrating_psy/data/sub-${SUBJ_NUM_4D}/*.csv ecco:/archival/projects/SPLaT/data/beh/sub-${SUBJ_NUM_4D}/raw/
+    scp ~/OneDrive\ -\ Emory\ University/SPLaT/data/controlled/sub-${SUBJ_NUM_4D}/*.csv ecco:/archival/projects/SPLaT/data/beh/sub-${SUBJ_NUM_4D}/raw/
+    scp ~/OneDrive\ -\ Emory\ University/SPLaT/data/naturalistic/sub-${SUBJ_NUM_4D}/*.csv ecco:/archival/projects/SPLaT/data/beh/sub-${SUBJ_NUM_4D}/raw/
 done
