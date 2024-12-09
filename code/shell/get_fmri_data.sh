1efe3704576818b4ff3a4189f6aeb726b5af1be5
monicathieu <mthieu@emory.edu>
2024-12-09 13:26:24 -0500

    rsync verbose now to see progress

--- code/shell/get_fmri_data.sh
+++ code/shell/get_fmri_data.sh
@@ -2,4 +2,4 @@
 read -p "Enter subj number to pull (no leading 0s): " SUBJ_NUM
 printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
 
-rsync -r mthieu@csic.som.emory.edu:/home/mthieu/kragel_data/FERN-KRAGELSPLAT/splat_${SUBJ_NUM_4D}* /archival/projects/SPLaT/data/fmri/dicom/
+rsync -r -v mthieu@csic.som.emory.edu:/home/mthieu/kragel_data/FERN-KRAGELSPLAT/splat_${SUBJ_NUM_4D}* /archival/projects/SPLaT/data/fmri/dicom/
