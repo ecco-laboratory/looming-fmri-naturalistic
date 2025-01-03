e940680af57055639a6d06d479b90ab5e57a11f4
monicathieu <mthieu@emory.edu>
2025-01-03 14:37:39 -0500

    attempt to pull multiple subjs serially

--- code/shell/get_fmri_data.sh
+++ code/shell/get_fmri_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-read -p "Enter subj number to pull (no leading 0s): " SUBJ_NUM
-printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
+read -p "Enter subj num(s) to pull (no leading 0s yes spaces): " SUBJ_NUMS
 
-rsync -r -v mthieu@csic.som.emory.edu:/home/mthieu/kragel_data/FERN-KRAGELSPLAT/splat_${SUBJ_NUM_4D}* /archival/projects/SPLaT/data/fmri/dicom/
+for SUBJ_NUM in ${SUBJ_NUMS[@]}; do
+    printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
+
+    rsync -r -v mthieu@csic.som.emory.edu:/home/mthieu/kragel_data/FERN-KRAGELSPLAT/splat_${SUBJ_NUM_4D}* /archival/projects/SPLaT/data/fmri/dicom/
+done
