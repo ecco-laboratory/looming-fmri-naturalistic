cff18f27e10bb37c1f97d88edc1448e22ec67bf6
monicathieu <mthieu@emory.edu>
2025-05-08 16:22:49 -0400

    take subj num as arg

--- code/shell/run_fmriprep.sh
+++ code/shell/run_fmriprep.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # this script takes the array ID that's going into sbatch_fmriprep as a positional argument
+read -p "Enter subj num to fmriprep with no leading 0s: " SUBJ_NUM
+
 SCRIPT_DIR=/home/data/eccolab/SPLaT_fMRI/code/shell
 
-SBATCH_OUT_FMRIPREP=$(sbatch --array=$1 $SCRIPT_DIR/sbatch_fmriprep.sh)
-JOB_ID=${SBATCH_OUT_FMRIPREP: -5}_$1
+SBATCH_OUT_FMRIPREP=$(sbatch --array=$SUBJ_NUM $SCRIPT_DIR/sbatch_fmriprep.sh)
+JOB_ID=${SBATCH_OUT_FMRIPREP: -5}_${SUBJ_NUM}
 echo "${SBATCH_OUT_FMRIPREP}: main fmriprep job"
 SBATCH_OUT_MONITOR=$(sbatch $SCRIPT_DIR/monitor_fmriprep.sh $JOB_ID)
 echo "${SBATCH_OUT_MONITOR}: monitoring job"
