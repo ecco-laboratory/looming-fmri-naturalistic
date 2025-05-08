16d5a31035c2f31c7efac4a129bf2ef3093c7d56
monicathieu <mthieu@emory.edu>
2025-05-08 16:21:50 -0400

    use direct path to kragel on csic

--- code/shell/get_fmri_data.sh
+++ code/shell/get_fmri_data.sh
@@ -5,4 +5,4 @@ read -p "Enter your csic.som.emory.edu username: " CSIC_USERNAME
 # so if there is any data in the CSIC folder you don't want to download... DEAL WITH IT! 
 # it may take a while to index the files at the beginning but then the --ignore-existing flag should prompt it to download only new subjects' data
 # so it won't re-download the entire project's worth of dicoms every time
-rsync -av --ignore-existing ${CSIC_USERNAME}@csic.som.emory.edu:/home/${CSIC_USERNAME}/kragel_data/FERN-KRAGELSPLAT/ /archival/projects/SPLaT/data/fmri/dicom/
+rsync -av --ignore-existing ${CSIC_USERNAME}@csic.som.emory.edu:/data2/rawdata/kragel/FERN-KRAGELSPLAT/ /archival/projects/SPLaT/data/fmri/dicom/
