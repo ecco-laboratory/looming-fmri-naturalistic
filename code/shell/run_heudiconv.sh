32f7aaa31310441a0f931861f61f781c7e12727a
monicathieu <mthieu@emory.edu>
2025-05-08 16:21:23 -0400

    only run on one subj at a time no loop

--- code/shell/run_heudiconv.sh
+++ code/shell/run_heudiconv.sh
@@ -5,17 +5,16 @@
 # Monica tried setting it up to run through slurm and it appeared to convert the data okay
 # but then didn't update the participants.tsv file with the finished subjects??
 
-read -p "Enter subj num(s) to heuristic with no leading 0s (but with spaces): " SUBJ_NUMS
+read -p "Enter subj num to heuristic with no leading 0s (but with spaces): " SUBJ_NUM
 
-for SUBJ_NUM in ${SUBJ_NUMS[@]}; do
-    printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
-    singularity run -B /archival/projects/SPLaT/data/fmri:/base \
-    /home/data/shared/SingularityImages/heudiconv_1.0.0.sif \
-    -d /base/dicom/splat_{subject}*/*/*.dcm \
-    -o /base/nifti \
-    -f /base/heuristic_default.py \
-    -s $SUBJ_NUM_4D \
-    -c dcm2niix \
-    -b \
-    --overwrite
-done
+printf -v SUBJ_NUM_4D '%04d' $SUBJ_NUM
+singularity run -B /archival/projects/SPLaT/data/fmri:/base \
+/home/data/shared/SingularityImages/heudiconv_1.0.0.sif \
+-d /base/dicom/splat_{subject}*/*/*.dcm \
+-o /base/nifti \
+-f /base/heuristic_default.py \
+-s $SUBJ_NUM_4D \
+-c dcm2niix \
+-b \
+--overwrite
+
