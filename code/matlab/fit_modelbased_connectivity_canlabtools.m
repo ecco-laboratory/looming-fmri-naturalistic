ef5b03ddcef768f3b36528c98ec8062b2d411615
monicathieu <mthieu@emory.edu>
2025-05-09 14:33:25 -0400

    clean implementation, read one subj at a time

--- code/matlab/fit_modelbased_connectivity_canlabtools.m
+++ code/matlab/fit_modelbased_connectivity_canlabtools.m
@@ -3,35 +3,34 @@
 addpath('/home/data/eccolab/Code/GitHub/spm12'); % per spm docs, do not genpath it
 addpath(genpath('/home/data/eccolab/Code/GitHub/CanlabCore'));
 
-% this script ONLY processes studyforrest data
-% and the SPM preprocessing script upstream of this will only output the realigned/normalized niftis
-% back to the dir where the original niftis are saved
-% so we are left going into the studyforrest folder
-studyforrest_dir = '../ignore/datasets/studyforrest-data-phase2';
 
 %% variables that must be set in targets before the script is called
+% THIS SCRIPT IS DESIGNED TO CALCULATE MODEL-BASED FUNCTIONAL CONNECTIVITY FOR A **SINGLE** HELD-OUT SUBJECT
+% It takes in cross-validated model-based seed timecourses that have already been fit over all subjects, 
+% so this script only needs to read in one subject at a time and train-test split separation can still be maintained. 
+% which is GOOD because whew mama, the whole brain fmri_data object gets untenably large when reading in more than a few subjects at once.
+% a downstream target will combine subjects' connectivity maps for t-value estimation.
+% subj_num: integer of subject ID. **NOT** the cross-validation fold number (so that it will be robust to potential changes in fold structure)
 % trs_to_use: vector of valid TR indices for SPM indexing into the 4D volume
 % tr_duration: in seconds, for the band-pass filter preproc
 % paths_nifti: derived from target. 
-% nested cell array of paths to smoothed timeseries niftis
+% cell array of paths to one subject's smoothed timeseries niftis
 % paths_confounds: derived from target. 
-% nested cell array to txts of SPM-compatible 2D matrices of confound regressors of interest
+% cell array to txts of SPM-compatible 2D matrices of confound regressors of interest
 % already had the initial volumes of each removed
-% BOTH paths_nifti and paths_confounds here must contain ALL SUBJECTS DATA: nested FIRST by SUBJECT and THEN by RUN
+% BOTH paths_nifti and paths_confounds here must contain SINGLE SUBJECT DATA: nested by RUN
 % seed_yhat_path: path to tabular data file containing cross-validated model-predicted multivoxel timecourses from seed region
+% this will be for ALL SUBJECTS and then this script will filter it for the applicable subject
 % out_path: full csv path to save the subj x whole-brain correlations out
 
-n_subjs = length(paths_nifti);
-n_runs = length(paths_nifti{1});
-
-%% EMERGENCY SETTING THOSE VARIABLES IF NOT RUNNING FROM TARGETS
-% sc_data_path = fullfile(studyforrest_dir, 'fmri_data_canlabtooled_sc.mat');
-% studyforrest_activation_path = fullfile(studyforrest_dir, 'flynet_convolved_timecourses.csv');
-% out_fstring = '%smap_flynet_connectivity_contrast.nii';
+n_runs = length(paths_nifti);
 
 %% LOAD IN DATA FROM TARGETS PATHS
 % model-predicted multivoxel timecourse for a given seed region and encoding model
 % THE SEED ROI AND ENCODING MODEL ARE IMPLICITLY BY THE PATH TO THE TIMECOURSE
+% this is the output written to out_path_pred by fit_pls_canlabtools, written out for _all subjects_
+% so you will need to read in the same file every time for each connectivity subject but that's fine bc it's fast
+
 % 2025-02-18: the first column of this is now subject indices
 seed_yhat = readmatrix(seed_yhat_path);
 % average the predicted response across "voxels" of the seed region 
@@ -41,38 +40,24 @@ seed_yhat_mean = squeeze(mean(seed_yhat(:, 2:end),2));
 %% READ AND CANLABTOOLS-PREPROCESS EVERYONE'S WHOLE BRAIN DATA :(
 
 % fencepost for first subject so we can preallocate the whole-brain matrix
-bold_preprocessed = preproc_fmri_data_canlabtools(trs_to_use, n_runs, tr_duration, paths_nifti{1}, paths_confounds{1});
+bold_preprocessed = preproc_fmri_data_canlabtools(trs_to_use, n_runs, tr_duration, paths_nifti, paths_confounds);
 dat_preprocessed = bold_preprocessed.dat';
-bold_height = height(dat_preprocessed);
-
-WB_DATA = zeros(n_subjs*bold_height, width(dat_preprocessed));
+dat_preprocessed = zscore(dat_preprocessed);
 
 subj_indices = seed_yhat(:, 1); % may as well since it already exists
-WB_DATA(subj_indices==1, :) = zscore(dat_preprocessed);
 
-% loop over the rest of the subjects
-for s=2:n_subjs
-    clear bold_preprocessed dat_preprocessed
-    bold_preprocessed = preproc_fmri_data_canlabtools(trs_to_use, n_runs, tr_duration, paths_nifti{s}, paths_confounds{s});
-    dat_preprocessed = bold_preprocessed.dat';
-    % no masking. whole brain. not parcel-averaged. all voxels. huge.
-    WB_DATA(subj_indices==s, :) = zscore(dat_preprocessed);
-end
-clear bold_preprocessed dat_preprocessed
-
-%% CORRELATE MODEL-SEED TIMECOURSE WITH WHOLE-BRAIN DATA, BY SUBJECT
-
-% leave-one-subject-out cross-validated functional connectivity. great
-vox_corr_wb = zeros(n_subjs, width(WB_DATA));
-
+%% CORRELATE THIS SUBJECT'S MODEL-SEED TIMECOURSE WITH THEIR WHOLE-BRAIN DATA
 fprintf('Correlating whole-brain data with model-seed timecourse...\n')
-for s = 1:n_subjs
-    % correlate the model-predicted seed timecourse with each whole-brain voxel timecourse
-    % you have to loop over subjects to get the prediction for each subject. 
-    % if you just corr on the whole thing it gives you the seed vox-target vox correlation across all subjects
-    % we now need to save the mean y-hats out for the permutation testing later as well
-    vox_corr_wb(s,:) = corr(seed_yhat_mean(subj_indices==s), WB_DATA(subj_indices==s,:));
-end
+% first pull the subj number from the BOLD path
+[~, file_nifti, ~] = fileparts(paths_nifti{1});
+% the substring positions assume it starts with the prefix "smoothed_4mm_"
+this_subj_num = str2double(extractBetween(convertCharsToStrings(file_nifti), 18, 21));
+
+% need to index seed_yhat_mean correctly to get just the relevant subject's model-predicted seed timecourse
+% based on matrix dimensions, this correlation will come out as a row vector with voxels on the columns
+vox_corr_wb = corr(seed_yhat_mean(subj_indices==this_subj_num), dat_preprocessed);
+% NB! transpose to column vector for writing out because R packages will read this in a little better later
+vox_corr_wb = vox_corr_wb';
 
 %% write out whole-brain correlations. to tabular data though
 % do the between model diffs in another script. this operates on one encoding model at a time
