761baf2fcab30c350bcb16a651aacfc1a8896251
monicathieu <mthieu@emory.edu>
2025-05-09 14:38:12 -0400

    add functions for all subjs noise measures

--- code/R/parse-confounds.R
+++ code/R/parse-confounds.R
@@ -2,9 +2,18 @@
 
 ## get path to fmriprep confounds file from subject/run/task ----
 
+# this gets for a single subject/run, for the main processing targets
 get_raw_confounds <- function (subject, task, run) {
   inject(here::here(!!!path_here_derivatives, subject, "func",
                     paste(subject, task, run, "desc-confounds_timeseries.tsv", sep = "_")))
+}
+
+# this gets for all fmriprepped subjects, for the QC targets kept separate (to include subjects excluded from main processing)
+get_all_raw_confounds <- function (task = "controlled") {
+  list.files(inject(here::here(!!!path_here_derivatives)), 
+             pattern = sprintf("(.*)task-%s(.*)desc-confounds_timeseries.tsv$", task), 
+             recursive = TRUE, 
+             full.names = TRUE)
 }
 
 ## targets-friendly: pull just the realignment motion parameter confounds ----
@@ -33,7 +42,37 @@ get_confounds_motion <- function (file_path, tr_duration, disdaq_duration) {
   return (out_path)
 }
 
-## targets-friendly: estimate dvars outlierness for a single run ----
+## targets-friendly and helper: select desired confound columns ----
+get_labeled_noise_measures_by_subject <- function (file_path, tr_duration, disdaq_duration) {
+  filename <- basename(file_path)
+  bids_parts <- str_split_1(filename, pattern = "_")
+  # skip out for pilot subjects
+  if (startsWith(bids_parts[1], "sub-99")) return (NULL)
+  
+  this_subj <- bids_parts[1] %>% 
+    str_split_1(pattern = "-") %>% 
+    pluck(2) %>% 
+    as.integer()
+  
+  this_task <- bids_parts[2] %>% 
+    str_split_1(pattern = "-") %>% 
+    pluck(2)
+  
+  this_run <- bids_parts[3] %>% 
+    str_split_1(pattern = "-") %>% 
+    pluck(2) %>% 
+    as.integer()
+  
+  out <- get_noise_measures(file_path,
+                            tr_duration = tr_duration,
+                            disdaq_duration = disdaq_duration) %>% 
+    mutate(subj_num = this_subj, 
+           run_num = this_run,
+           task = this_task)
+  
+  return (out)
+}
+
 get_noise_measures <- function (file_path, tr_duration, disdaq_duration) {
   confounds <- read_confounds(file_path = file_path, 
                               tr_duration = tr_duration, 
