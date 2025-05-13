ef0c902dedafe6c016e561a4cdb93eff565d3b80
monicathieu <mthieu@emory.edu>
2025-05-13 15:39:25 -0400

    remove --fs-license-file flag lol

--- code/shell/sbatch_fmriprep.sh
+++ code/shell/sbatch_fmriprep.sh
@@ -31,8 +31,6 @@ export SINGULARITYENV_TEMPLATEFLOW_HOME="/templateflow"
 SINGULARITY_CMD="singularity run --cleanenv --bind ${BIDS_DIR}:/data --bind /home/data/shared/SingularityImages/:/fslicensepath --bind ${TEMPLATEFLOW_HOST_HOME}:${SINGULARITYENV_TEMPLATEFLOW_HOME}  --bind ${STUDY_DIR}/ignore/tmp:/work  /home/data/shared/SingularityImages/fmriprep-23.1.4.simg"
 
 # echo Current slurm array task ID: $SLURM_ARRAY_TASK_ID
-# Parse the participants.tsv file and extract one subject ID from the line corresponding to this SLURM task.
-# subject=$( sed -n -E "$((${SLURM_ARRAY_TASK_ID} + 1))s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv )
 printf -v subject "sub-%04d" $SLURM_ARRAY_TASK_ID
 echo Running for subject: $subject
 # Remove IsRunning files from FreeSurfer
@@ -40,12 +38,13 @@ echo Running for subject: $subject
 
 # Compose the command line
 # flags you may or may not want to use:
+# --fs-license-file /fslicensepath/license.txt if you ARE trying to use reconall
 # --fs-no-reconall (default would be to run it)
 # flag notes
 # --nprocs is the total number of threads
 # --omp-nthreads is the total number of threads per process
 # I think we don't use the --mem flag because that is capped by the memory requested in the sbatch header?
-cmd="${SINGULARITY_CMD} /data /data/${DERIVS_DIR} participant --participant-label $subject --fs-license-file /fslicensepath/license.txt --fs-no-reconall -w /work/ -vv --nprocs 32 --omp-nthreads 8  --output-spaces MNI152NLin2009cAsym:res-2 anat fsaverage5 --verbose --low-mem --me-output-echos"
+cmd="${SINGULARITY_CMD} /data /data/${DERIVS_DIR} participant --participant-label $subject --fs-no-reconall -w /work/ -vv --nprocs 32 --omp-nthreads 8  --output-spaces MNI152NLin2009cAsym:res-2 anat fsaverage5 --verbose --low-mem --me-output-echos"
 
 
 # Setup done, run the command
