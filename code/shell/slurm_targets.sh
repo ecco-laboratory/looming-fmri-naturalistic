1ca9c3afb01ef7dadc3b0409fe043202146e8a4b
monicathieu <mthieu@emory.edu>
2025-07-08 02:09:17 -0400

    take tar_proj and tidyselect contains args

--- code/shell/slurm_targets.sh
+++ code/shell/slurm_targets.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #
-#SBATCH --nodelist=node1,node4
+#SBATCH --account=default
+#SBATCH --exclude=gpu1,gpu2
 #SBATCH --time=24:00:00
+#SBATCH --mem=32G
 #SBATCH --partition day-long
 # Outputs ----------------------------------
 #SBATCH -o /home/%u/log/%x-%j.out
 #SBATCH -e /home/%u/log/%x-%j.err
 # ------------------------------------------
 
+# takes the TAR_PROJECT as a positional arg now!
 cd /home/data/eccolab/SPLaT_fMRI
 # edit the R code in the Rscript string below depending on what targets you need to run
-Rscript -e "targets::tar_make(starts_with('level2'),use_crew=FALSE)"
+# random notes:
+# the targets that call canlabtools preproc (bold.masked and wb.model.connectivity by-subject targets) require like 32 GB memory
+Rscript -e "Sys.setenv(TAR_PROJECT='${1}'); library(targets); tar_make(contains('${2}'), use_crew=FALSE)"
